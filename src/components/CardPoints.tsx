d92b3665c21a86e83e5339e7ecf54305cad4524c
juandevsigner <juanda.core07@gmail.com>
2022-10-20 18:25:29 -0500

    ajuste iconos

--- src/components/CardPoints.tsx
+++ src/components/CardPoints.tsx
@@ -7,9 +7,9 @@ interface Props {
 export const CardPoints = ({ count, name, img }: Props) => {
   return (
     <div className="text-center">
-      <div className="bg-green-500 rounded-full p-5  flex flex-col items-center justify-center">
-        <img className="w-1/5 text-center" src={img} alt="imagen-product" />
-        <p className=" text-4xl text-white font-semibold text-center">
+      <div className="bg-green-500 rounded-full p-7  flex flex-col items-center justify-center">
+        <img className=" w-14 text-center" src={img} alt="imagen-product" />
+        <p className=" text-5xl text-white font-semibold text-center">
           {count}
         </p>
       </div>
