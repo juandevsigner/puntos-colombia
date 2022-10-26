f45128c79a05b596a4f81e2a9bbd08b03e4449ef
juandevsigner <juanda.core07@gmail.com>
2022-10-25 20:22:37 -0500

    video add

--- src/components/Publicity.tsx
+++ src/components/Publicity.tsx
@@ -1,9 +1,16 @@
-import PublicityImage from "../assets/publi.webp";
+import ReactPlayer from "react-player";
 
 export const Publicity = () => {
   return (
     <div className="bg-green-100 flex-1 overflow-hidden">
-      <img className="w-full h-full" src={PublicityImage} alt="publicity" />
+      <ReactPlayer
+        playing={true}
+        url="https://www.youtube.com/embed/hyjO0Un20cc"
+        width="100%"
+        height="100%"
+        loop={true}
+        muted={true}
+      />
     </div>
   );
 };
